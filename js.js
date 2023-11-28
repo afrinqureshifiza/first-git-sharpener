295ea01c5dda5b8c4abd4061b38f28bb2ff52a02
Afrin Qureshi <afrinqureshifiza@gmail.com>
2023-11-28 11:28:39 +0530

    queryselector added

--- js.js
+++ js.js
@@ -1,5 +1,13 @@
-let third=document.getElementById("third");
-third.style.backgroundColor="green";
+// let third=document.getElementById("third");
+// third.style.backgroundColor="green";
 
-let listItem=document.getElementById("items");
-listItem.style.fontWeight="bold";
+// let listItem=document.getElementById("items");
+// listItem.style.fontWeight="bold";
+
+// let list=document.getElementsByTagName('li');
+// list[4].style.border="black"
+
+let item=document.querySelectorAll('.list-group-item');
+item[1].style.color="green";
+item[0].style.backgroundColor="green"
+item[2].style.backgroundColor="green"
