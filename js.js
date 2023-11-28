690e9b20ee957647f66a5274d10482da863d8707
Afrin Qureshi <afrinqureshifiza@gmail.com>
2023-11-28 20:48:53 +0530

    delete btn added

--- js.js
+++ js.js
@@ -14,29 +14,74 @@
 // item[0].style.backgroundColor="green"
 // item[2].style.backgroundColor="green"
 
-let item=document.querySelector('#items');
-console.log(item.parentElement.parentElement.parentElement.parentElement.parentElement)
-item.parentElement.style.backgroundColor="yellow"
+// let item=document.querySelector('#items');
+// console.log(item.parentElement.parentElement.parentElement.parentElement.parentElement)
+// item.parentElement.style.backgroundColor="yellow"
 
-console.log(item.children)
-console.log(item.firstChild)
-console.log(item.lastChild)
-console.log(item.firstElementChild)
-item.firstElementChild.textContent="hello"
-item.lastElementChild.textContent="bye"
-console.log(item.nextElementSibling)
-console.log(item.previousElementSibling)
-let newdiv=document.createElement("div")
-newdiv.className='hello'
-newdiv.id='hello'
-newdiv.setAttribute('title','hello div')
-// newdiv.textContent='inside new div'
-let text=document.createTextNode('text element');
-newdiv.appendChild(text)
-console.log(newdiv)
-// document.body.appendChild(newdiv)
+// console.log(item.children)
+// console.log(item.firstChild)
+// console.log(item.lastChild)
+// console.log(item.firstElementChild)
+// item.firstElementChild.textContent="hello"
+// item.lastElementChild.textContent="bye"
+// console.log(item.nextElementSibling)
+// console.log(item.previousElementSibling)
+// let newdiv=document.createElement("div")
+// newdiv.className='hello'
+// newdiv.id='hello'
+// newdiv.setAttribute('title','hello div')
+// // newdiv.textContent='inside new div'
+// let text=document.createTextNode('text element');
+// newdiv.appendChild(text)
+// console.log(newdiv)
+// // document.body.appendChild(newdiv)
 
-let container=document.querySelector('header .container')
-let h1=document.querySelector('header h1')
-console.log(h1)
-container.insertBefore(newdiv, h1)
+// let container=document.querySelector('header .container')
+// let h1=document.querySelector('header h1')
+// console.log(h1)
+// container.insertBefore(newdiv, h1)
+// let item1=document.querySelector('li')
+// item.insertBefore(newdiv,item1)
+
+
+//delete button
+let form=document.getElementById('addform');
+let itemlist=document.getElementById('items');
+ 
+//form submit event
+form.addEventListener('submit',additem);
+
+//delete event
+// let delitem=document.getElementsByClassName('btn')
+// delitem.addEventListener('click',deleteitem)
+itemlist.addEventListener('click',deleteitem)
+
+function deleteitem(e){
+    if(e.target.classList.contains('delete')){
+      if(confirm('are you sure?')){
+    let li=e.target.parentElement;
+   itemlist.removeChild(li) 
+ }  
+    }
+ 
+}
+
+function additem(event){
+  event.preventDefault();
+  console.log(1);
+  //get input value
+let newitem =document.getElementById('item').value
+
+//create new li element
+let li=document.createElement('li')
+let btn=document.createElement('button') 
+btn.className='btn float-right btn-danger btn-sm delete'
+btn.textContent='X'
+li.className='list-group-item'
+// li.appendChild(document.createTextNode(newitem))
+li.textContent=newitem
+li.appendChild(btn)
+itemlist.appendChild(li);
+}
+
+
